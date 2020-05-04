14c4ef3cc327e254760cf255901eb2f913ea3e9e
Daniel Moya <daniel.moya@knowtrade.eu>
2020-05-04 17:19:09 +0200

    20200504171909601368700

--- lightweight/leanxcale/lxs_start.bash
+++ lightweight/leanxcale/lxs_start.bash
@@ -6,32 +6,59 @@ function dn() { return 0; } #Do Nothing.
 function fdt() { date +%Y%m%d%H%M:%S:%N; }
 function output() { echo -e "`fdt`:\e[92m$@\e[0m"; }
 
-export BASEDIR=/lxs/v0_400 #LeanXcale Home.
-export BACKUPDIR=/backup
-
-cd $BASEDIR && tar xvf /tmp/$SW_NAME
-
 ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""
 
 ssh-agent bash
 ssh-add ~/.ssh/id_rsa
+
+output "-- LeanXcale Software (LXS) Install --"
+
+export BASEDIR=/lxs/v0_400 #LeanXcale Home.
+
+SW_NAME=LeanXcale_0.400-SNAPSHOT_latest.tgz
+
+cd $BASEDIR && tar xvf /tmp/$SW_NAME
+
+sed -i "/PyYAML/d" $BASEDIR/LX-BIN/scripts/requirements.txt
+
+pip3 install -r $BASEDIR/LX-BIN/scripts/requirements.txt
+ansible --version
+
+export PATH=$PATH:/home/lxs/.local/bin
 
 cd $BASEDIR
 source ./env.sh
 
 echo "source ./env.sh" >> ~/.bash_profile
 
-#wget inventory.
+output "-- Getting inventory file from private repo --"
+
+wget --no-check-certificate https://ca355c55-0ab0-4882-93fa-331bcc4d45bd.pub.cloud.scaleway.com:3000/danimoya/wget-artifacts/raw/master/lightweight/leanxcale/inventory \
+    -O $BASEDIR/conf/inventory
+
+output "-- Deploying the configuration --"
 
 ./admin/deploycluster.sh
+
+output "-- Starting the Database Software --"
 
 ./admin/startcluster.sh
 
 # lxConsole 3    	# Show components.
 # lxClient  			# SQL CLI.
 
+output "-- Creating DB & DBA User --"
+
 uuidgen |  tr -d "-" > leanxcale_password
 PWD_LXS=`cat leanxcale_password`
+DB_NAME=leanxcale
+DB_USER=lxdba
+${BASEDIR}/LX-BIN/bin/lxClient << EOF 
+  !connect jdbc:leanxcale://localhost:1529/${DB_NAME} ${DB_USER} ${PWD_LXS}
+  !tables
+  !quit
+EOF
+
 #lxClient<<EOF
 #!connect jdbc:leanxcale://localhost:1529/lxs1 lxadm $PWD_LXS
 #EOF
@@ -40,3 +67,5 @@ PWD_LXS=`cat leanxcale_password`
 # readonly_password >> EMAIL.
 
 # qe-driver-0.400-20200403.113453-83.jar
+
+output "-- Finished! --"
--- lightweight/leanxcale/root_prepare_lxs_lw.bash
+++ lightweight/leanxcale/root_prepare_lxs_lw.bash
@@ -5,13 +5,21 @@
 
 set -eu
 
+function dn() { return 0; } #Do Nothing.
+function fdt() { date +%Y%m%d%H%M:%S:%N; }
+function output() { echo -e "`fdt`:\e[92m$@\e[0m"; }
+
 SW_NAME=LeanXcale_0.400-SNAPSHOT_latest.tgz
+
+output "-- Updating & Installing necessary packages --"
 
 apt-get update -y
 
 apt-get install -y curl python3-dev libffi-dev libssl-dev \
 libxml2-dev openssh-client locate vim tree rsync nux-tools \
 libxslt1-dev libjpeg8-dev zlib1g-dev python3-distutils wget gcc screen numactl
+
+output "-- Downloading OpenJDK 1.8 --"
 
 wget https://cdn.azul.com/zulu/bin/zulu8.46.0.19-ca-jdk8.0.252-linux_amd64.deb
 
@@ -22,41 +30,40 @@ apt -y autoremove
 export JAVA_HOME=/usr/lib/jvm/zulu-8-amd64
 java -version
 
+output "Downloading LeanXcale Latest Software from GDrive"
+
 #Download LeanXcale latest software.
 wget --progress=dot:mega --load-cookies /tmp/cookies.txt \
 "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1VZYkOkUmZ2k32cf7H0PXYpLi6ow2EPTS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VZYkOkUmZ2k32cf7H0PXYpLi6ow2EPTS" \
 -O /tmp/$SW_NAME && rm -rf /tmp/cookies.txt
 chmod 755 /tmp/$SW_NAME
 
+output "-- Adding Users --"
+
 groupadd -g 500 dba
 useradd -g dba lxs
+mkdir /home/lxs && chown lxs:dba /home/lxs
+usermod --shell /bin/bash lxs
 
 useradd -c 'Mostly R/O Account' roaccount
+mkdir /home/roaccount && chown roaccount:dba /home/roaccount
 usermod -G dba roaccount
+usermod --shell /bin/bash roaccount
 uuidgen |  tr -d "-" > readonly_password
 echo "roaccount:`cat readonly_password`" | chpasswd
 
+output "-- Creating SW Directories --"
+
 export BASEDIR=/lxs/v0_400 #LeanXcale Home.
-export BACKUPDIR=/backup
 mkdir -p $BASEDIR
 mkdir -p /backup
 chown -R lxs:dba /lxs
 
+output "-- Getting PIP3 --"
+
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
+#export PATH=$PATH:/usr/local/bin/pip3
 
-export PATH=$PATH:/usr/local/bin/pip3
-
-sed -i "/PyYAML/d" $BASEDIR/LX-BIN/scripts/requirements.txt
-
-#gcc required for netifaces module.
-pip3 install -r $BASEDIR/LX-BIN/scripts/requirements.txt
-ansible --version
-
-ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""
-
-ssh-agent bash
-ssh-add ~/.ssh/id_rsa
-
-#su - lxs -c "wget lxs_start.bash"
-#su - lxs -c "chmod +x lxs_start.bash && bash lxs_start.bash"
+su - lxs -c "wget --no-check-certificate https://ca355c55-0ab0-4882-93fa-331bcc4d45bd.pub.cloud.scaleway.com:3000/danimoya/wget-artifacts/raw/master/lightweight/leanxcale/lxs_start.bash -O /home/lxs/lxs_start.bash"
+su - lxs -c "chmod +x /home/lxs/lxs_start.bash && ./home/lxs/lxs_start.bash"
