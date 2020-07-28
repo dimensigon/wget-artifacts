ea8878b9429d165bcb7ddcc24eda1e5f8a9b6524
Daniel Moya <daniel.moya@knowtrade.eu>
2020-07-28 16:32:37 +0200

    20200728163237334540600

--- init_scripts/just-join.bash
+++ init_scripts/just-join.bash
@@ -11,19 +11,38 @@ echo "
 #!/bin/bash
 # Dimensigon - Generate a Cloud-init including a token.
 
+set -eu
+
+function fdt() { date +%Y%m%d%H%M:%S:%N; }
+function output() { echo -e \"`fdt`:\e[92m$@\e[0m\"; }
+
+output \"-- Creating Dimensigon OS User --\"
+
 useradd -s /bin/bash -m dimensigon
 
-apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
+output \"-- Installing the necessary packages --\"
+
+apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
+
+output \"-- Python - Creating a Virtual Environment --\"
 
 su - dimensigon -c \"python -m venv -- prompt dimensigon venv\"
 
+output \"-- Python - Autoload at login time --\"
+
 su - dimensigon -c \"echo 'source ~/venv/bin/activate' >> .bash_profile\"
 
+output \"-- PIP Install Dimensigon --\"
+
 su - dimensigon -c \"pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple dimensigon\"
+
+output \"-- Show me the date --\"
 
 echo \"`date +%Y%m%d%H%M%S%N`\"
 
 date +%Y%m%d%H%M%S%N
+
+output \"-- D I M E N S I G O N = Joining the server $SERVER:$PORT --\"
 
 su - dimensigon -c \"dimensigon join --port $PORT $SERVER $TOKEN\"
 
